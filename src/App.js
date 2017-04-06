67bc7d43e29c176eb6e7e8384d6d531f8b81cedd
Wang Dongfang <wangdongfang@WangdeMac-mini.local>
2017-04-06 14:27:05 +0800

    解决冲突

--- src/App.js
+++ src/App.js
@@ -8,14 +8,7 @@ import createSagaMiddleware from 'redux-saga'
 import Home from './router/homesever'
 import Active from './router/activesever'
 import reducers from './reducers'
-<<<<<<< HEAD
-import createSagaMiddleware from 'redux-saga'
 import sagas from './saga/index'
-const sagaMiddleware=createSagaMiddleware();
-=======
-import req from './request/fenth'
-import saga from './sagas'
->>>>>>> fcf37b561fdef880ce97c0f62d2eb4b75b284638
 const history = createHistory();
 const middleware = routerMiddleware(history);
 const sagaMiddleware = createSagaMiddleware()
@@ -26,13 +19,7 @@ const store = createStore(
     }),
     applyMiddleware(middleware,sagaMiddleware)
 )
-<<<<<<< HEAD
 sagaMiddleware.run(sagas);
-=======
-sagaMiddleware.run(saga)
-store.dispatch({type:'TEST',params:'你好'})
-store.dispatch({type:'TEST1',params:'我好'})
->>>>>>> fcf37b561fdef880ce97c0f62d2eb4b75b284638
 class app extends Component{
     render(){
         return(
