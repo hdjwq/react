ad42a044525fcc55a63d413fbf96fa0778c4c058
ran <ranyun@bao.cn>
2017-05-10 16:39:32 +0800

    sss

--- src/pages/home/home/index.js
+++ src/pages/home/home/index.js
@@ -2,10 +2,34 @@
  * Created by Administrator on 2017/5/8.
  */
 import React,{Component} from 'react'
-
+import cns from 'classnames'
+import './index.less'
 class Index extends Component{
+    state={
+        listArry:['1'],
+    }
+    componentDidMount(){
+        this.timeAdd();
+    }
+    timeAdd=()=>{
+        let a=1;
+        setTimeout(()=>{
+            a++;
+            const {listArry}=this.state;
+            listArry.push(a)
+            this.setState({
+                listArry
+            })
+        },5000)
+    }
     render(){
-        return(<div>我是主页</div>)
+        const {listArry,flage}=this.state;
+        return(<div>
+            我是主页
+            {listArry.map((item,i)=>(
+                <div key={i} className={cns('list')}>{item}</div>
+            ))}
+        </div>)
     }
 }
 export default Index
